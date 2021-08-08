7f90e8c75e22ac97e3abf3a1a98945a69d25a151
Dan <daniel.giddins112@gmail.com>
2021-08-08 18:06:44 +0100

    use new namespace

--- BikePoint/Program.cs
+++ BikePoint/Program.cs
@@ -1,8 +1,8 @@
-﻿using System;
+﻿using RestHelperLib;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using UoN.Library.Api.RestHelper;
 
 namespace BikePoint
 {
